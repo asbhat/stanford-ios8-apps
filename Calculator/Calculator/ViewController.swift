4298de02330bbab237aa2377e289704cc0741ef4
asbhat <aditya.s.bhat@gmail.com>
2015-02-17 22:20:18 +0100

    added buttons and display updates

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
+// this is a bad name for a controller, but keeping it at default for now...
 class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        // the exclamation point (!) unwraps the Optional (will crash if is Nil)
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
