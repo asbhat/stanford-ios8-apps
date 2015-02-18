73eebce8cb1466baff35768255eb48a153f60e2a
asbhat <aditya.s.bhat@gmail.com>
2015-02-18 23:35:01 +0100

    working with most buttons

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 // this is a bad name for a controller, but keeping it at default for now...
 class ViewController: UIViewController {
+    // the exclamation point (!) while declaring means always automatically unwrap
+    // "implicitly unwrapped optional"
+    // mostly useful when a variable is set very early and stays set
     @IBOutlet weak var display: UILabel!
-
-    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    // if a type can be inferred, let it (no need for ': Bool')
+    var userIsInTheMiddleOfTypingANumber = false
     
     @IBAction func appendDigit(sender: UIButton) {
         // the exclamation point (!) unwraps the Optional (will crash if is Nil)
@@ -26,6 +30,52 @@ class ViewController: UIViewController {
         }
     }
     
-
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        // switch labels variables passed in as $n
+        case "×": performOperation {$0 * $1}
+        case "÷": performOperation {$1 / $0}
+        case "+": performOperation {$0 + $1}
+        case "−": performOperation {$1 - $0}
+        case "√": performOperation {sqrt($0)}
+        default: break
+        }
+    }
+    
+    // function type here "takes in two Doubles and returns a Double"
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+    }
+    
+    // "computed property" = instance variables that are computed rather than stored
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 }
 
