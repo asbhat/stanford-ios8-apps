943120c61d163b7ecfba2096522915670a45579d
asbhat <aditya.s.bhat@gmail.com>
2015-12-14 23:56:30 -0500

    Model initial commit

---
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -0,0 +1,103 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Aditya Bhat on 12/14/15.
+//  Copyright © 2015 Aditya Bhat. All rights reserved.
+//
+
+import Foundation
+
+
+// This is the 'Model' in MVC
+class CalculatorBrain {
+    
+    
+    private enum Op : CustomStringConvertible {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        // read only, so no 'set'
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
+    }
+    
+    private var opStack = [Op]()
+    private var knownOps = [String: Op]()
+    
+    
+    // Can't simplify divide and minus because operations are pulled off the stack backwards
+    init () {
+        func learnOp(op : Op) {
+            knownOps[op.description] = op
+        }
+        
+        learnOp(Op.BinaryOperation("×", *))
+        learnOp(Op.BinaryOperation("÷"){ $1 / $0 })
+        learnOp(Op.BinaryOperation("+", +))
+        learnOp(Op.BinaryOperation("−"){ $1 - $0 })
+        learnOp(Op.UnaryOperation("√", sqrt))
+        learnOp(Op.UnaryOperation("sin", sin))
+        learnOp(Op.UnaryOperation("cos", cos))
+    }
+    
+    // arrays are structs and are passed by value
+    // all functions make parameters immutable by default, unless you put a 'var' before them in the function defintion
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            // '_' is a way to signify you are ignoring a parameter
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluate = evaluate(remainingOps)
+                if let operand1 = op1Evaluate.result {
+                    let op2Evaluate = evaluate(op1Evaluate.remainingOps)
+                    if let operand2 = op2Evaluate.result {
+                        return (operation(operand1, operand2), op2Evaluate.remainingOps)
+                    }
+                }
+            }
+        }
+        
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        print ("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String) -> Double? {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+}
